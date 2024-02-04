7f3176ec94bfebd5af7cd976c9f39f713ba8f98b
Muhammad Azka <hello.mazka@gmail.com>
2024-02-04 21:43:34 +0700

    feat: added theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html data-theme="corporate" lang="en">
       <body className={inter.className + " h-screen w-screen"}>{children}</body>
     </html>
   );
